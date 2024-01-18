4a8cffaa7ff3b6b6b1f408bac90a60c24a9be05b
041090 <akanksha.sharma231@gmail.com>
2024-01-18 19:29:51 +0530

    new code added

---
+++ classquestion.js
@@ -0,0 +1,11 @@
+const firstname=["harkirat" ,"akanksha" ,"bhushan" ];
+
+/*console.log(firstname[0]);
+
+console.log(firstname[1]);
+
+console.log(firstname[2]);*/
+
+for(let i =0 ;i < firstname.length ; i++){
+    console.log(firstname[i]);
+}
